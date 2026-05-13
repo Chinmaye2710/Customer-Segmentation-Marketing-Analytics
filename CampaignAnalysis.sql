2507ba37bc6ab95ea1aad1a710273f883f24e182
Chinmaye2710 <chinmayekasarla27@gmail.com>
2026-05-13 19:35:46 +0530

    Delete CampaignAnalysis.sql

--- CampaignAnalysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-	Segment,
-	COUNT(*) AS CustomerCount,
-	SUM(Response) AS Total_Responders,
-	COUNT(*)- SUM(Response) AS UnRespondedCustomers,
-	SUM(Response) *100/COUNT(*) AS ResponseRate
-FROM Customer1
-GROUP BY Segment 
-ORDER BY ResponseRate DESC 
